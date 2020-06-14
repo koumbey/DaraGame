081ba642130c76690200d89243290e606dc076cf
Issoufou Galadima Djadi <issoufou.galadima.djadi@eisti.fr>
2020-06-14 14:13:15 +0200

    remove pusher

--- dara-backend/config/config.js
+++ dara-backend/config/config.js
@@ -1,10 +1,3 @@
 module.exports ={
-    JWT_SECRET:"dara@90_haoussa_bakwoy",
-    PUSHER_CONFIG:{
-        appId: 'dara-webSocket',
-        key: 'dara@key_app',
-        secret: 'dara_uku@haoussa_bakwoy',
-        cluster: 'dara_webSocket_cluster',
-        encrypted: true
-    }
+    JWT_SECRET:"dara@90_haoussa_bakwoy"
 };
